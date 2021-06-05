9f73188aaf3d8e4ef0a4d09a657ba1070331415e
MrGabin <79531012+GabinCleaver@users.noreply.github.com>
2021-06-05 17:49:40 +0200

    Update pierre_feuille_ciseaux.py

--- pierre_feuille_ciseaux.py
+++ pierre_feuille_ciseaux.py
@@ -1,4 +1,7 @@
 import random
+from colorama import Fore, init
+
+init()
 
 replay = True
 
@@ -6,7 +9,7 @@ while replay == True:
 
     # Initalisations:
 
-    choice_user = input("Entrez votre choix (pierre, feuille, ciseaux): ")
+    choice_user = input(Fore.LIGHTCYAN_EX + "Entrez votre choix (pierre, feuille, ciseaux): ")
     choice_computer = random.randint(1,3)
 
     # Conversion des chiffre en valeur dures, pierre pour 1 etc...
@@ -21,34 +24,34 @@ while replay == True:
     # Vérifications des possiblitées:
 
     if choice_user == choice_computer:
-        print(f"""Egalité !
+        print(Fore.LIGHTYELLOW_EX + f"""Egalité !
         Voici le résultat: 
         Toi: {choice_user}
         Ordinateur: {choice_computer}""")
     elif choice_user == "pierre" and choice_computer == "feuille":
-        print(f"""Défaite !
+        print(Fore.LIGHTRED_EX + f"""Défaite !
         Voici le résultat: 
         Toi: {choice_user}
         Ordinateur: {choice_computer}""")   
     elif choice_computer == "pierre" and choice_user == "feuille":
-        print(f"""Victoire !
+        print(Fore.LIGHTGREEN_EX + f"""Victoire !
         Voici le résultat: 
         Toi: {choice_user}
         Ordinateur: {choice_computer}""")  
     elif choice_user == "ciseaux" and choice_computer == "feuille":
-        print(f"""Victoire !
+        print(Fore.LIGHTGREEN_EX + f"""Victoire !
         Voici le résultat: 
         Toi: {choice_user}
         Ordinateur: {choice_computer}""")     
     elif choice_computer == "ciseaux" and choice_user == "feuille":
-        print(f"""Défaite !
+        print(Fore.LIGHTRED_EX + f"""Défaite !
         Voici le résultat: 
         Toi: {choice_user}
         Ordinateur: {choice_computer}""")
 
     # Rejouer:
 
-    replay = input("""Rejouer ? (y/n) 
+    replay = input(Fore.WHITE + """Rejouer ? (y/n) 
     [>] """)
     if replay == "y":
         replay = True
